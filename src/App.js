cc00f8ac79d30273f02f9e863725c293225efe36
Junaid Arafeen <junaid.arafeen@bestbuy.com>
2017-05-26 09:14:16 -0500

    added initial component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      Hi
-      </div>
+      <SearchBar />
     );
   }
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
+  render() {
+    return  (
+      <div>
+      <input
+        value={this.state.term}
+        onChange={event => this.setState({term : event.target.value})}/>
+      Value: {this.state.term}
+      </div>
+
+    );
+  }
+}
+
+export default SearchBar;
